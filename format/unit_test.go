9670da64f893f2f75dddc25938c8f48022e73b94
Gregory Vincic <g@7de.se>
2020-01-11 14:38:33 +0100

    Add examples

--- format/unit_test.go
+++ format/unit_test.go
@@ -1,6 +1,9 @@
 package format
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_level_funcs(t *testing.T) {
 	ok := func(got, exp string) {
@@ -9,10 +12,21 @@ func Test_level_funcs(t *testing.T) {
 			t.Error("\n", got, " not equal to\n", exp)
 		}
 	}
-	ok(Debug("x"), "format/unit_test.go:12: x")
-	ok(Debugf("debug x"), "format/unit_test.go:13: debug x")
+	ok(Debug("x"), "format/unit_test.go:15: x")
+	ok(Debugf("debug x"), "format/unit_test.go:16: debug x")
 
 	ok(Info("x"), "x")
 	ok(Infof("info %s", "x"), "info x")
+}
 
+func ExampleDebug() {
+	fmt.Println(Debug("hello"))
+	// output:
+	// format/unit_test.go:23: hello
+}
+
+func ExampleDebugf() {
+	fmt.Println(Debugf("Hello, %s", "world!"))
+	// output:
+	// format/unit_test.go:29: Hello, world!
 }
