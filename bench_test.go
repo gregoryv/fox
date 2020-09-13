f58d1009f91250a053c054f445e537ba6c983667
Gregory Vincic <g@7de.se>
2020-09-13 06:34:04 +0200

    Bench test log + format

--- bench_test.go
+++ bench_test.go
@@ -1,37 +1,43 @@
-package format
+package fox_test
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
+
+	"github.com/gregoryv/fox"
+	"github.com/gregoryv/fox/format"
 )
 
 func BenchmarkConfig_Info(b *testing.B) {
-	cnf := NewConfig()
+	cnf := format.NewConfig()
 	cnf.Stamp()
 	cnf.SetPrefix("BENCH:")
 	err := fmt.Errorf("bench err message")
+	var buf bytes.Buffer
+	log := fox.NewSyncLog(&buf).FilterEmpty().Log
 
 	b.Run("Info", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			cnf.Info("info", err)
+			log(cnf.Info("info", err))
 		}
 	})
 
 	b.Run("Infof", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			cnf.Infof("%s %v", "infof", err)
+			log(cnf.Infof("%s %v", "infof", err))
 		}
 	})
 
 	b.Run("Debug", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			cnf.Debug("debug", err)
+			log(cnf.Debug("debug", err))
 		}
 	})
 
 	b.Run("Debugf", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			cnf.Debugf("debugf %v", err)
+			log(cnf.Debugf("debugf %v", err))
 		}
 	})
 
