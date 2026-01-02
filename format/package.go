99f205de099b973d006c71286921375a31325b9f
Gregory Vincic <gregoryvse@gmail.com>
2026-01-02 08:49:34 +0100

    gofumpt

--- format/package.go
+++ format/package.go
@@ -1,21 +1,20 @@
 /*
 Package format generates strings for logging.
 
-      import (
-         "github.com/gregoryv/fox"
-         . "github.com/gregoryv/fox/format"
-      )
+	import (
+	   "github.com/gregoryv/fox"
+	   . "github.com/gregoryv/fox/format"
+	)
 
-      Log := fox.NewSyncLog(os.Stdout).Log
-      Log(Debugf("failed: %v", err))
+	Log := fox.NewSyncLog(os.Stdout).Log
+	Log(Debugf("failed: %v", err))
 
 Debug and Debugf include the name of file and line number of the call
 location similar but not the same as using the builtin log package
 with Lshortfile
 
-      log := New(os.Stdout, "", Lshortfile)
-      log.Printf("failed: %v", err)
-
+	log := New(os.Stdout, "", Lshortfile)
+	log.Printf("failed: %v", err)
 */
 package format
 
--- package.go
+++ package.go
@@ -3,27 +3,26 @@ Package fox provides loggers implementing the simple fox.Logger interface.
 The fox.Logger interface matches that of testing.T.Log method which
 makes it very easy to inject during testing.
 
-
-Example usage
+# Example usage
 
 Use the Log method as a first class citizen
 
-  Log := fox.NewSyncLog(os.Stdout).Log
-  Log("some", "nice", "message")
+	Log := fox.NewSyncLog(os.Stdout).Log
+	Log("some", "nice", "message")
 
 and warnings are simplified by filtering out empty values
 
-  warn := fox.NewSyncLog(os.Stdout).FilterEmpty().Log
-  warn("") // will not be logged
+	warn := fox.NewSyncLog(os.Stdout).FilterEmpty().Log
+	warn("") // will not be logged
 
-  // Log errors only if there are any
-  warn(nil) // nothing, it's nil
-  warn(io.EOF)
+	// Log errors only if there are any
+	warn(nil) // nothing, it's nil
+	warn(io.EOF)
 
 Wrap the standard log package and it's default logger
 
-  Log := fox.LoggerFunc(log.Println)
-  Log("hello", "standard", "logger")
+	Log := fox.LoggerFunc(log.Println)
+	Log("hello", "standard", "logger")
 */
 package fox
 
